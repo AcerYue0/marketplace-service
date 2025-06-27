1494099079220395bc145f77c912dd620accae95
JLee <pkh_aceryue@photons.com.tw>
2025-06-27 14:30:28 +0800

    renew getList response

--- task-service/src/main/java/com/example/controller/GetDataController.java
+++ task-service/src/main/java/com/example/controller/GetDataController.java
@@ -2,9 +2,12 @@ package com.example.controller;
 
 import com.example.enums.Setting;
 import com.example.task.GetNFTCollectionItemsValueTask;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/data")
@@ -19,9 +22,8 @@ public class GetDataController {
      * 取得當前的列表
      */
     @PostMapping("/getList")
-    public String manualTrigger() {
+    public Map<String, Object> manualTrigger() {
         Setting.GLOBAL_LOGGER.info("[manualTrigger]");
-        task.executeGetListTask();
-        return "Get all item price in NFT collections complete.";
+        return task.executeGetListTask();
     }
 }
